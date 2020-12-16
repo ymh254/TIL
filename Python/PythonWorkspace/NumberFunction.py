fdba89599bb74d676705333a8c2ba0af0090deef
ymh254 <ymh254@gmail.com>
2020-12-16 16:33:46 +0900

    add PythonWorkspace

---
+++ Python/PythonWorkspace/NumberFunction.py
@@ -0,0 +1,17 @@
+print(abs(-5))  # Absolute value: 5
+print(pow(4, 2))  # 4^2 = 4*4 = 16
+print(max(5, 12))  # 12
+print(min(5, 12))  # 5
+print(round(3.14))  # 3
+
+print(randrange(1, 11))  # 1 to 10
+print(randint(1, 10))  # 1 to 10
+
+# from math import *
+print(floor(4.99))  # 4
+print(ceil(3.14))  # 4
+print(sprt(16))  # 4
+
+# from random import *
+print(random())  # random value from 0.0 to less than 1.0
+print(int(random()*10))  # interger from 0 to 9
